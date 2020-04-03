b26b50a19e9d4c9f42dbd39ce1794b5954a2d108
Sui.Young <suisauce.codes@gmail.com>
2020-04-03 14:29:36 -0400

    function for keyswap works, refactored how the recipeApp object was being written by using const, so the whole object isn't being reqritten

--- scripts/search.js
+++ scripts/search.js
@@ -1,28 +1,55 @@
-const recipeApp = {
-    key: '0d411c50c97a49d5a155391721a6abea',
-};
-
-recipeApp.userDiet = '';
-//150 points perday
+// Spoonacular API procides 150 points free, perday
 // Sui's Key : baa233e2a8bc401a83b89ba0f32ef23c
-//Sui's Secondary Key: 279c2442197649fa90e84de120dfa672
-//Sui's Third Key : ffbaefcb24f942e3b26825d47ad292b0
+// Sui's Secondary Key: 279c2442197649fa90e84de120dfa672
+// Sui's Third Key : ffbaefcb24f942e3b26825d47ad292b0
 // Sui's Fourth key: 03807c83cc6546a980c784079a8c2fd8 
 // Sui's Fifth key: a8e92198263545bcb214ec6e78a03c7f 
-//Naveen's Key : e429c44d3e5e48beacacf5b14cc993a2
-//Naveen's Secondary Key : 0d411c50c97a49d5a155391721a6abea
+// Naveen's Key : e429c44d3e5e48beacacf5b14cc993a2
+// Naveen's Secondary Key : 0d411c50c97a49d5a155391721a6abea
 
+// ** each key produces 7 clicks of functionality for this app. with 8 keys,we will be able to get 56 clicks per day, instead of 7. Add more keys for better functionality.
+
+//function to loop through keys per click:
+const recipeApp = {};
+let keys = [
+    "baa233e2a8bc401a83b89ba0f32ef23c",
+    "279c2442197649fa90e84de120dfa672",
+    "0d411c50c97a49d5a155391721a6abea",
+    "ffbaefcb24f942e3b26825d47ad292b0",
+    "03807c83cc6546a980c784079a8c2fd8",
+    "a8e92198263545bcb214ec6e78a03c7f",
+    "e429c44d3e5e48beacacf5b14cc993a2",
+    "0d411c50c97a49d5a155391721a6abea"
+    ],
+    counter = 0;
+
+console.log('initialkey', keys[counter]); //initial value
+
+recipeApp.key= keys[counter];
+
+recipeApp.keySwap = () => {
+    counter = (counter + 1) % keys.length
+    console.log('increment counter', keys[counter]); 
+    //increment counter
+    //modulus (%) operater resets the counter to 0 when it reaches the length of the array; thus restarting the count whenever the counter(index) reaches the end of the array.
+    
+    recipeApp.key = keys[counter];
+    
+    console.log("recipeApp.key", recipeApp.key); //key currently being used, 
+};
+
+
+recipeApp.userDiet = '';
 
 // Get recipes with user ingredients input (Whatever they have)
 recipeApp.getrecipes = function (ingredientInput) {
-
     // Ajax request 1 [will retrieve IDs needed for recipe info]
     $.ajax({
         url: `https://api.spoonacular.com/recipes/findByIngredients`,
         method: 'GET',
         dataType: 'json',
         data: {
-            apiKey: recipeApp.key,
+            apiKey: recipeApp.key, //key grabbed from the function on click for the submit button.
             ingredients: ingredientInput
         }
     }).then(function (result) {
@@ -38,7 +65,7 @@ recipeApp.getrecipes = function (ingredientInput) {
 
                 // Capturing ID of all the recipes in a variable and popping it in the link
                 const recipeId = eachRecipe.id;
-
+                console.log('recipeId', recipeId)
                 //Make ajax call with new end point with recipeID to gain recipe info on url, dietary restrictions, instructions and etc..
                 //Ajax 2
                 $.ajax({
@@ -83,16 +110,15 @@ recipeApp.getrecipes = function (ingredientInput) {
                             
                         }
                     }
-
                 })
             });
         }
     });
-
 }
 
+
 recipeApp.init = function () {
-    recipeApp.getrecipes();
+    // recipeApp.getrecipes();
 
     // Function to get user input through the search box and pass that as an argument in the function recipeApp.getrecipes(ingredientInput);
     $('.searchBoxClass').on('submit', function (event) {
@@ -112,19 +138,21 @@ recipeApp.init = function () {
             scrollTop: $("main").offset().top
         },
             'slow');
+
+        //swap key
+        recipeApp.keySwap();
     })
 }
+
 
 //clear results and scroll to top
 $('.reloadAll').on('click', function (e) {
     e.preventDefault();
-    // location.reload(true);
-    $('.searchBoxClass').trigger('reset');
+    $('.searchBoxClass').trigger('reset'); //reset form inputs
     $('ul')
-        .removeClass('suggestedRecipes')
+        .removeClass('suggestedRecipes') //hide placeholders
         .addClass('emptySuggestions');
-        console.log($(".inputBox").val());
-        
+    
 })
 
 $("a[href^='#']").click(function(e) {
